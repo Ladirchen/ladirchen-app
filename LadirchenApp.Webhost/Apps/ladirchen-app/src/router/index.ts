14713126948c1c4c588534c007063b6395f03892
Renáta Irinyi <103267773+ReniIrinyi@users.noreply.github.com>
2026-08-31 18:16:00 +0200

    #5: feat: launch the family world shell and onboarding

--- LadirchenApp.Webhost/Apps/ladirchen-app/src/router/index.ts
+++ LadirchenApp.Webhost/Apps/ladirchen-app/src/router/index.ts
@@ -5,11 +5,11 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Index from "@/pages/index.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
